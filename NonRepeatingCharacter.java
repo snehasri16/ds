9559854036e0cff5bc5c7037dce999997b768bc1
snehasri16 <117506229+snehasri16@users.noreply.github.com>
2023-07-22 19:36:50 +0530

    Add files via upload

---
+++ NonRepeatingCharacter.java
@@ -0,0 +1,22 @@
+package com.dp;
+import java.util.*;
+public class NonRepeatingCharacter {
+    static char nonrepeatingCharacter(String s){
+        int[] h=new int[26];
+        Arrays.fill(h,0);
+        for(int i=0;i<s.length();i++){
+            h[s.charAt(i)-'a']+=1;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(h[s.charAt(i)-'a']==1){
+                return s.charAt(i);
+            }
+        }
+        return '$';
+    }
+    public static void main(String[] args) {
+        String s="hellooo";
+        System.out.println(nonrepeatingCharacter(s));
+    }
+}
